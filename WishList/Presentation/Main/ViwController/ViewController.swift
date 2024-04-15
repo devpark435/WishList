f0c0d40f068ad1e6fb5df5809f5a3fbec3e9e117
devpark435 <devpark435@gmail.com>
2024-04-15 20:30:48 +0900

    feat #2 - TestCode Get API

--- WishList/Presentation/Main/ViwController/ViewController.swift
+++ WishList/Presentation/Main/ViwController/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ProductAPIManager.shared.fetchProduct(id: 1) { result in
+            switch result {
+            case .success(let product):
+                // product 인스턴스를 사용하여 작업 수행
+                print(product)
+            case .failure(let error):
+                // 에러 처리
+                print(error.localizedDescription)
+            }
+        }
     }
 
 
