bcc80b5ceb1881f9a87a31e579047df6a4c50657
devpark435 <devpark435@gmail.com>
2024-04-19 20:58:54 +0900

    feat #10 - Cell imageView contentMode scaleAspectFill

--- WishList/Presentation/WishList/View/SelectedProductCell.swift
+++ WishList/Presentation/WishList/View/SelectedProductCell.swift
@@ -13,7 +13,7 @@ import CoreData
 class SelectedProductCell: UITableViewCell{
     
     let productImageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFit
+        $0.contentMode = .scaleAspectFill
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 16
     }
